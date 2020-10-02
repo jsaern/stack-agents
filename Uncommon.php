222ce84b576c11b1cb17c57dddc7205599063ece
VE7DET <jneri@uniserve.com>
2020-10-01 19:28:32 -0700

    workaround for multiple files

--- Uncommon.php
+++ Uncommon.php
@@ -1,4 +1,7 @@
 <?php
+
+// setup
+
 namespace Nrwtaylor\StackAgentThing;
 
 ini_set('display_startup_errors', 1);
@@ -13,9 +16,10 @@ class Uncommon extends Agent
 
     function init()
     {
-
-$this->minimum_word_length = 4;
+        $this->minimum_word_length = 4;
     }
+
+    // functions ----- ----- ----- -----
 
     function run()
     {
@@ -24,75 +28,75 @@ $this->minimum_word_length = 4;
 
     public function doUncommon()
     {
-    $this->commonwordsUncommon();
-   
-    $file = "/home/jsae/Outputs/independence/word-sources/articles/20200903-I5-2-buildinggreen-Passive Survivability A New Design Criterion for Buildings.txt";
-    $data = file_get_contents($file,true);
-    //var_dump("merp");
-    //var_dump($data);
- $words = [];
- $arr = explode("\n", $data);
- foreach($arr as $key=>$line) {
- //var_dump($line);
- $tokens = explode(" ", $line);
- foreach($tokens as $j=>$token) {
- 
- // Strip out numbers and spaces
- $word_name = trim(strtolower($token));
- $word_name = trim($word_name,";"); 
- $word_name = trim($word_name," ");
- $word_name = trim(preg_replace('/\t+/','', $word_name));
+        $this->commonwordsUncommon();
+        // Bring file in to assess and create set of words to check
+$file = "/home/jsae/Outputs/independence/word-sources/articles/articletest.txt";
+        $data = file_get_contents($file, true);
+        //var_dump("merp");
+        //var_dump($data);
+        $words = [];
+        $arr = explode("\n", $data);
+        foreach ($arr as $key => $line) {
+            //var_dump($line);
+            $tokens = explode(" ", $line);
+            foreach ($tokens as $j => $token) {
+                // Strip out numbers and spaces
+                $word_name = trim(strtolower($token));
+                $word_name = trim($word_name, ";");
+                $word_name = trim($word_name, " ");
+                $word_name = trim(preg_replace('/\t+/', '', $word_name));
 
-if (is_numeric($word_name)) {continue;}
+                if (is_numeric($word_name)) {
+                    continue;
+                }
 
-if ( strlen($word_name) <= $this->minimum_word_length ) {continue;}
- 
- if (!isset($words[$word_name])) {$words[$word_name] = 0;}
- $words[$word_name] += 1;
- 
- }
- }
+                if (strlen($word_name) <= $this->minimum_word_length) {
+                    continue;
+                }
 
-// Tag types of word
+                if (!isset($words[$word_name])) {
+                    $words[$word_name] = 0;
+                }
+                $words[$word_name] += 1;
+            }
+        }
 
-$this->brilltagger_agent = new Brilltagger($this->thing, "brilltagger");
-$allowed_tags = ['NN','VBN','JJ'];
-//echo "\n"; 
- foreach($words as $word_name=>$word) {
- 
- //var_dump("---");
- //var_dump($word_name);
-$respond = $this->isUncommon($word_name); 
- 
- if ($respond === true) {
- 
+        // Tag types of word
 
+        $this->brilltagger_agent = new Brilltagger($this->thing, "brilltagger");
+        $allowed_tags = ['NN', 'VBN', 'JJ'];
+        //echo "\n";
+        foreach ($words as $word_name => $word) {
+            //var_dump("---");
+            //var_dump($word_name);
+            $respond = $this->isUncommon($word_name);
 
-$tags = $this->brilltagger_agent->tag($word_name);
+            if ($respond === true) {
+                $tags = $this->brilltagger_agent->tag($word_name);
 
+                $tag = $tags[0]['tag'];
+                if (in_array($tags[0]['tag'], $allowed_tags)) {
+                    $this->response .= $word_name . " ";
+                    //var_dump($tags[0]['tag']);
+                }
+            }
+        }
 
- $tag = $tags[0]['tag'];
- if (in_array($tags[0]['tag'], $allowed_tags)) {
- 
-$this->response .= $word_name . " ";
- //var_dump($tags[0]['tag']);
- 
- }
- 
- }
- 
- }
- 
- //echo "\n";
-// corncob_lowercase.txt
+        //couple of lines of crude debugging code
+        //echo "\n";
+        // corncob_lowercase.txt
 
-    
+        // ---
+        // Respond to caller
+
         if ($this->agent_input == null) {
-            $array = array('miao', 'miaou', 'hiss', 'prrr', 'grrr');
+            $array = ['miao', 'miaou', 'hiss', 'prrr', 'grrr'];
             $k = array_rand($array);
             $v = $array[$k];
 
             $response = "UNCOMMON | " . strtolower($v) . ".";
+            // Save words
+            //file_put_contents("/home/jsae/2018/outputs/organizations/2020-orgfinder/words/passive_survivability.txt",$v);
 
             $this->uncommon_message = $response; // mewsage?
         } else {
@@ -100,47 +104,49 @@ $this->response .= $word_name . " ";
         }
     }
 
-public function isUncommon($text = null) {
+    public function isUncommon($text = null)
+    {
+        // Identify uncommon words in the set
+        // based on length of dictionary used
+        if ($text == null) {
+            return;
+        }
+        foreach ($this->common_words as $i => $common_word) {
+            if (strtolower($text) == strtolower($common_word)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-if ($text == null) {return;}
-foreach($this->common_words as $i=>$common_word) {
+    public function commonwordsUncommon()
+    {
+        //alternative dictionary lists for common words
+        //var_dump($this->resource_path);
+        //$data = file_get_contents($this->resource_path . 'corncob/corncob_lowercase.txt');
+        //$data = file_get_contents($this->resource_path . 'corncob/corncob_lowercase.txt');
+        $dict_path = '/home/jsae/Outputs/independence/word-sources/dict/';
+        $data = file_get_contents($dict_path . 'freq10000-en-list.txt');
 
-if (strtolower($text) == strtolower($common_word)) {return false;}
+        //var_dump($data);
 
+        $words = [];
+        $arr = explode("\n", $data);
+        foreach ($arr as $key => $line) {
+            //var_dump($line);
+            $tokens = explode(" ", $line);
+            foreach ($tokens as $j => $token) {
+                $token = trim($token);
 
-}
-return true;
-}
+                //$word_name = strtolower($token);
 
-public function commonwordsUncommon() {
-//var_dump($this->resource_path);
-//$data = file_get_contents($this->resource_path . 'corncob/corncob_lowercase.txt');
-//$data = file_get_contents($this->resource_path . 'corncob/corncob_lowercase.txt');
-$dict_path = '/home/jsae/Outputs/independence/word-sources/dict/';
-$data = file_get_contents($dict_path . 'freq10000-en-list.txt');
+                //if (!isset($words[$word_name])) {$words[$word_name] = 0;}
+                $words[] = $token;
+            }
+        }
 
-//var_dump($data);
-
-$words = [];
- $arr = explode("\n", $data);
- foreach($arr as $key=>$line) {
- //var_dump($line);
- $tokens = explode(" ", $line);
- foreach($tokens as $j=>$token) {
- 
- $token = trim($token);
- 
- //$word_name = strtolower($token);
- 
- //if (!isset($words[$word_name])) {$words[$word_name] = 0;}
- $words[] = $token;
- 
- }
- }
-
-$this->common_words = $words;
-
-}
+        $this->common_words = $words;
+    }
 
     function getNegativetime()
     {
@@ -170,11 +176,11 @@ $this->common_words = $words;
 
     function makeSMS()
     {
-        $this->node_list = array("uncommon" => array("cat", "dog"));
- 
-$sms = "Found uncommon words: ". $this->response;
- 
-        $this->sms_message = $sms; 
+        $this->node_list = ["uncommon" => ["cat", "dog"]];
+
+        $sms = "Found uncommon words: " . $this->response;
+
+        $this->sms_message = $sms;
         $this->thing_report['sms'] = $sms;
     }
 
